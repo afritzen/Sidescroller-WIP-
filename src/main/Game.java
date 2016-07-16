f38e87c6a1e9c9396c5308a9367836c19b8c4858
Anna Fritzen <afritzen@techfak.uni-bielefeld.de>
2016-07-16 15:00:33 +0200

    created project, entry points and first packages

---
+++ src/main/Game.java
@@ -0,0 +1,21 @@
+package main;
+
+import javax.swing.*;
+
+/**
+ * Entry point for the game. Here, the JFrame is initialized.
+ */
+public class Game {
+
+    /**
+     * Title screen.
+     */
+    private JFrame window;
+
+    public static void main(String[] args) {
+
+
+
+    }
+
+}
