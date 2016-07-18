cb6bd8434067fbd8307e9ea7a54c7da9951963e3
Anna Fritzen <afritzen@techfak.uni-bielefeld.de>
2016-07-18 18:46:08 +0200

    finished TileMap, first level is now loaded when choosing 'Start'

--- src/gamestate/Level1State.java
+++ src/gamestate/Level1State.java
@@ -1,5 +1,8 @@
 package gamestate;
 
+import main.GamePanel;
+import tilemap.TileMap;
+
 import java.awt.*;
 
 /**
@@ -7,15 +10,25 @@ import java.awt.*;
  */
 public class Level1State extends GameState{
 
-    //private TileMap tileMap;
+    private TileMap tileMap;
 
+    /**
+     * Initializes the first level by loading the map and setting up the
+     * gamestate manager.
+     * @param gameStateManager manages current state
+     */
     public Level1State(GameStateManager gameStateManager) {
         this.gameStateManager = gameStateManager;
+        init();
     }
 
     @Override
     public void init() {
-
+        // init map
+        tileMap = new TileMap(30);
+        tileMap.loadTiles("/tilesets/grasstileset.gif");
+        tileMap.loadMap("/maps/level1-1.map");
+        tileMap.setPosition(0, 0);
     }
 
     @Override
@@ -25,7 +38,11 @@ public class Level1State extends GameState{
 
     @Override
     public void draw(Graphics2D graphics2D) {
-
+        //clear screen to be safe
+        graphics2D.setColor(Color.WHITE);
+        graphics2D.fillRect(0, 0, GamePanel.WIDTH, GamePanel.HEIGHT);
+        // draw map
+        tileMap.draw(graphics2D);
     }
 
     @Override
--- src/tilemap/TileMap.java
+++ src/tilemap/TileMap.java
@@ -3,7 +3,11 @@ package tilemap;
 import main.GamePanel;
 
 import javax.imageio.ImageIO;
+import java.awt.*;
 import java.awt.image.BufferedImage;
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 
 /**
  * Models a map in the game with an array of tiles and
@@ -107,7 +111,137 @@ public class TileMap {
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+    /**
+     * Loads a .map file and parses all information such as tile types and
+     * dimensions.
+     * @param filename name of the .map file
+     */
+    public void loadMap(String filename) {
+
+        try {
+
+            InputStream inputStream = getClass().getResourceAsStream(filename);
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+            // set dimensions from first two rows
+            numCols = Integer.parseInt(bufferedReader.readLine());
+            numRows = Integer.parseInt(bufferedReader.readLine());
+            map = new int[numRows][numCols];
+            width = numCols*tileSize;
+            height = numRows*tileSize;
+
+            String delimiters = "\\s+";
+            // get sub-arrays as array of token
+            for (int row = 0; row < numRows; row++) {
+                String line = bufferedReader.readLine();
+                String tokens[] = line.split(delimiters);
+
+                // get value for every coordinate
+                for(int col =  0; col < numCols; col++) {
+                    map[row][col] = Integer.parseInt(tokens[col]);
+                }
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
     }
 
+    /**
+     * Returns the type of a tile in the graphical array.
+     * @param row x-position of tile
+     * @param col y-position of tile
+     * @return  type of the tile
+     */
+    public int getType(int row, int col) {
+        int value = map[row][col];
+        int r = value/numTilesAcross;
+        int c = value%numTilesAcross;
+        return tiles[r][c].getType();
+    }
+
+    /**
+     * Sets the player's position in the map and adds a smooth scrolling effect.
+     * Also, the offsets of row and column are calculated.
+     * @param xPos new x-position
+     * @param yPos new y-position
+     */
+    public void setPosition(double xPos, double yPos) {
+        this.xPos += (xPos - this.xPos)*tween;
+        this.yPos += (yPos - this.yPos)*tween;
+
+        fixBounds();
+        colOffset = (int)-this.xPos/tileSize;
+        rowOffset = (int)-this.yPos/tileSize;
+    }
+
+    /**
+     * Makes sure the bounds of the screen are not crossed.
+     */
+    public void fixBounds() {
+        if (xPos < xMin) {
+            xPos = xMin;
+        }
+        if (yPos < yMin) {
+            yPos = yMin;
+        }
+        if (xPos > xMax) {
+            xPos = xMax;
+        }
+        if (yPos > yMax) {
+            yPos = yMax;
+        }
+    }
+
+    /**
+     * Draws all components of the map.
+     * @param graphics2D graphics to be drawn
+     */
+    public void draw(Graphics2D graphics2D) {
+        for (int row = rowOffset; row < rowOffset+numRowsToDraw; row++) {
+
+            if (row >= numRows) {
+                break;
+            }
+
+            for (int col = colOffset; col < colOffset+numColsToDraw; col++) {
+
+                if (col >= numCols) {
+                    break;
+                }
+
+                if (map[row][col] == 0) {
+                    continue;
+                }
+
+                int value = map[row][col];
+                int r = value/numTilesAcross;
+                int c = value%numTilesAcross;
+
+                graphics2D.drawImage(tiles[r][c].getImage(), (int)xPos+col*tileSize, (int)yPos+row*tileSize, null);
+            }
+        }
+    }
+
+    public double getxPos() {
+        return xPos;
+    }
+
+    public double getyPos() {
+        return yPos;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public int getTileSize() {
+        return tileSize;
+    }
 }
